cb3e950003d9a7d22906c97521e41ef59329ef52
Igor Cesar <igorcesarhtml@gmail.com>
2024-01-29 08:11:13 -0300

    feat: create news card with react.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,15 @@
-import Navbar from "../../Components/Navbar/Navbar";
+import Card from "../../Components/Cards/Card.jsx";
+import news from "../../Datas.js";
+
+import Navbar from "../../Components/Navbar/Navbar.jsx";
 
 export default function Home() {
   return (
     <>
       <Navbar></Navbar>
+      {news.map((item, index) => {
+        return <Card key={index} news={item}></Card>;
+      })}
     </>
   );
 }
